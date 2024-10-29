885401fd42aa44319cd36012e909166e78540541
Artur <arturilamonta@gmail.com>
2024-10-29 12:25:29 +0200

    The registration system work and forgot the password also works

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,3 +6,14 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name = "signup")
 ]
+
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
+
+# идет автоматически если мы подключи path("accounts/", include("django.contrib.auth.urls"))
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUp
 from django.views import generic 
 from django.urls import reverse_lazy
 # Create your views here.
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUp
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
